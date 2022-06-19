46a24b683e8fe46aab40bbb8189e4a517744428c
cihatsolak <cihatsolak@hotmail.com>
2022-06-19 16:51:19 +0300

    Redux toolkit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.min.css'
 import { BrowserRouter } from 'react-router-dom';
+import store, { persistor } from "./store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 );
 
