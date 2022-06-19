7976dd79d2789053d6c794a882a66df5d924ac86
cihatsolak <cihatsolak@hotmail.com>
2022-06-19 17:20:45 +0300

    Refactoring: Post and PostCreate page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.min.css'
-import { BrowserRouter } from 'react-router-dom';
+import './index.css'
+import 'antd/dist/antd.css'
+import { BrowserRouter } from "react-router-dom"
 import store, { persistor } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
