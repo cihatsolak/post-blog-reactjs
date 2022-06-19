1926153cfc57a7485f9e1957bf93bd582c099705
cihatsolak <cihatsolak@hotmail.com>
2022-06-19 15:04:53 +0300

    Create axios request instance

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Router from './router/Router';
+import { useEffect } from 'react';
+import { request } from './request/Request';
 
 function App() {
+
+  useEffect(() => {
+    request.get('/test')
+  }, [])
+
   return (
     <>
       <Router />
